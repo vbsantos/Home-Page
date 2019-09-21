afc074dc55bbcca45d1b0c33dc72003069369be0
vbsantos <vbsanttos@gmail.com>
2019-09-21 15:58:49 -0300

    Primeiro commit

---
+++ script/main.js
@@ -0,0 +1,134 @@
+const sites = [
+    { id: 0, name: "Google", description: "Pesquisa qualquer coisa", link: ["https://www.google.com/", "https://www.google.com/search?q="], icon: "img/icon_google.png", active: true },
+    { id: 1, name: "Google Translator", description: "Traduz qualquer coisa", link: ["https://translate.google.com/", "https://translate.google.com/#auto/pt/"], icon: "img/icon_translator.png", active: true },
+    { id: 2, name: "YouTube", description: "Pesquisa vídeos", link: ["https://www.youtube.com/", "https://www.youtube.com/results?search_query="], icon: "img/icon_youtube.png", active: true },
+    { id: 3, name: "Bing", description: "Pesquisa qualquer coisa", link: ["https://www.bing.com/", "https://www.bing.com/search?q="], icon: "img/icon_bing.png", active: false },
+    { id: 4, name: "Wikipedia", description: "Sabe tudo sobre tudo", link: ["https://pt.wikipedia.org/", "https://pt.wikipedia.org/wiki/"], icon: "img/icon_wikipedia.png", active: true },
+    { id: 5, name: "GitHub", description: "Pesquisa códigos e projetos open-source", link: ["https://www.github.com/", "https://github.com/search?q="], icon: "img/icon_github.png", active: true },
+    { id: 6, name: "Stack Overflow", description: "Pesquisa problemas e soluções relacionadas a programação", link: ["https://pt.stackoverflow.com/", "https://pt.stackoverflow.com/search?q="], icon: "img/icon_stackoverflow.png", active: true },
+    { id: 7, name: "Google Images", description: "Pesquisa Imagens", link: ["https://www.google.com/", "https://www.google.com.br/images?q="], icon: "img/icon_google.png", active: false },
+    { id: 8, name: "IMDb", description: "Pesquisa filmes e séries", link: ["https://www.imdb.com/", "https://www.imdb.com/find?ref_=nv_sr_fn&q="], icon: "img/icon_imdb.png", active: true },
+    { id: 9, name: "Reddit", description: "Pesquisa subreddits", link: ["https://www.reddit.com/", "https://www.reddit.com/search/?q="], icon: "img/icon_reddit.png", active: false }
+];
+
+const search_bar = document.getElementById("form");
+
+search_bar.focus();
+
+const get_search = () => {
+    return search_bar.value;
+}
+
+const toInternet = (common_string) => {
+    return common_string.toLowerCase().replace(" ", "%20");
+}
+
+const setAsSearchOption = (site) => {
+    console.log("Setting \"" + site["name"] + "\" as search option.")
+    const searchIcons = document.getElementById("search-options");
+    const slot = document.createElement("div");
+    slot.classList.add("col-sm");
+    const icon = document.createElement("img");
+    icon.src = site["icon"];
+    icon.title = site["name"];
+    icon.alt = "icon";
+    icon.classList.add("search-icon");
+    slot.setAttribute("id", "si" + site["id"]); // si0 si1 si2
+    slot.appendChild(icon);
+    searchIcons.appendChild(slot);
+    slot.addEventListener("mousedown", function (e) {
+        const link = site["link"];
+        const search = get_search();
+        if (search == "") {
+            var address = link[0];
+        }
+        else {
+            var address = link[1] + toInternet(search);
+        }
+        if (e.which == 1) {
+            window.location.href = address;
+        }
+        else if (e.which == 2 || e.button == 4) {
+            window.open(address);
+        }
+    });
+}
+
+const removeSearchOption = (site) => {
+    const searchIcons = document.getElementById("search-options");
+    const searchOption = document.getElementById("si" + site["id"]);
+    searchIcons.removeChild(searchOption);
+}
+
+const makeSideMenuItem = (site) => {
+    console.log("Adding side menu item.");
+    const menu = document.getElementById("sidemenu");
+    const item = document.createElement("div");
+    item.title = site["description"];
+    item.classList.add("side-menu-item");
+    item.classList.add("row");
+    const icon = document.createElement("img");
+    icon.src = site["icon"]
+    icon.classList.add("col-sm-2");
+    const h = document.createElement("h2");
+    const title = document.createTextNode(site["name"]);
+    const link = document.createElement("a");
+    link.href = site["link"][0];
+    link.classList.add("col-sm-9");
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.classList.add("col-sm-1");
+    if (site["active"]) {
+        cb.checked = true;
+        setAsSearchOption(site);
+    }
+    cb.addEventListener("mousedown", function (e) {
+        if (e.which == 1) {
+            if (!cb.checked) {
+                // console.log("ATIVOU "+site["name"]);
+                setAsSearchOption(site);
+            }
+            else {
+                // console.log("DESATIVOU "+site["name"]);
+                removeSearchOption(site);
+            }
+        }
+    });
+    h.appendChild(title);
+    link.appendChild(h);
+    item.appendChild(cb);
+    item.appendChild(link);
+    item.appendChild(icon);
+    menu.appendChild(item);
+}
+
+sites.forEach(makeSideMenuItem);
+
+// ########################################################################################################
+
+const googletranslate = "https://translate.google.com/#auto/pt/";
+const google = "https://www.google.com/search?q=";
+const youtube = "https://www.youtube.com/results?search_query=";
+const bing = "https://www.bing.com/search?q=";
+const wikipedia = "https://pt.wikipedia.org/wiki/";
+const github = "https://github.com/search?q=";
+const stackoverflow = "https://pt.stackoverflow.com/search?q=";
+const imagens = "https://www.google.com.br/images?q=";
+const imdb = "https://www.imdb.com/find?ref_=nv_sr_fn&q="
+
+
+
+
+// const pesquisa = (site) => {
+//     console.log("pesquisa("+site+")");
+//     const search = get_search();
+//     if (search !== "") {
+//         const address = site + toInternet(search);
+//         console.log("Pesquisando: \"" + search + "\"\n" + address);
+//         // window.location.replace(address);      // não retorna pra pagina
+//         // window.location.href = address;        // retorna pra pagina mas não da pra usar o botão do meio do mouse
+//         window.open(address)                    // abre somente em nova guia
+//         return true;
+//     }
+//     return false;
+// }
