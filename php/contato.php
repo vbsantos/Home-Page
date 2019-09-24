b8e53e3859ab122090e0eb6be3944a54567c50aa
vbsantos <vbsanttos@gmail.com>
2019-09-23 22:47:15 -0300

    Backend quase funcional

--- php/contato.php
+++ php/contato.php
@@ -1,5 +1,24 @@
 <?php
 
-    
+    if(isset($_POST["email"]) && !empty($_POST["email"])) {
+        $nome = addslashes($_POST["nome"]);
+        $email = addslashes($_POST["email"]);
+        $mensagem = addslashes($_POST["mensagem"]);
+
+        $to = "vinicius.bohrer@ecomp.ufsm.br";
+        $subject = "Contato - Startpage";
+        $body = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Mensagem: " . $mensagem;
+        // O HEADER TEM QUE COMEÇAR COM FROM, OBRIGATORIAMENTE
+        // E O EMAIL TEM QUE PERTENCER AO DOMÍNIO NO QUAL O SCRIPT TA RODANDO
+        $header = "From: contato@startpage.com.br\n" . "Reply-To: " . $email . "\n" . "X=Mailer:PHP/" . phpversion();
+
+        echo("From:" . $nome . " (" . $email . ")\n" . "Message:" . $mensagem);
+
+        if(mail($to, $subject, $body, $header)) {
+            echo("E-mail enviado com sucesso.")
+        } else {
+            echo("Erro ao enviar e-mail.")
+        }
+    }
 
 ?>
