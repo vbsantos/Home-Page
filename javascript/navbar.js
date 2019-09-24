df9a6b9239e3a3ef221e90547eb03f8691fbbe0c
vbsantos <vbsanttos@gmail.com>
2019-09-23 21:30:18 -0300

    Troquei nome das pastas e adicionei backend em php

---
+++ javascript/navbar.js
@@ -0,0 +1,55 @@
+const desativaLink = (link) => {
+    if (link.classList.contains("active")) {
+        link.classList.remove("active");
+    }
+}
+
+const desativaLinks = (links) => {
+    links.forEach(desativaLink);
+}
+
+const fechaGuia = (aba) => {
+    aba.style.display = "none";
+}
+
+const fechaGuias = (abas) => {
+    abas.forEach(fechaGuia);
+}
+
+const ativaLink = (link) => {
+    link.classList.add("active");
+}
+
+const abreGuia = (link) => {
+    const id = link.id;
+    let new_id = id.substring(3, id.length);
+    new_id = "sidemenu" + new_id;
+    console.log("ID DA GUIA QUE DEVE SER ABERTA: " + new_id);
+    const aba = document.getElementById(new_id);
+    aba.style.display = "block";
+}
+
+const setChangeGuideEvent = (link) => {
+    link.addEventListener("mousedown", function (e) {
+        if (e.which == 1) {
+            let links = document.getElementsByClassName("nav-item");
+            links = [links[0], links[1], links[2], links[3]];
+            let abas = document.getElementsByClassName("menu");
+            abas = [abas[0], abas[1], abas[2], abas[3]];
+
+            if (!link.classList.contains("active")) {
+                desativaLinks(links);
+                fechaGuias(abas);
+                ativaLink(link);
+                abreGuia(link);
+            }
+        }
+    });
+}
+
+// ##########################################
+
+let links = document.getElementsByClassName("nav-item");
+links = [links[0], links[1], links[2], links[3]];
+
+links.forEach(setChangeGuideEvent);
