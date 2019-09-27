689a72585fcea79f2289d759c150de8c48cdfbbc
vbsantos <vbsanttos@gmail.com>
2019-09-27 18:44:20 -0300

    backend temporario

--- php/contato.php
+++ php/contato.php
@@ -1,24 +1,28 @@
 <?php
 
-    if(isset($_POST["email"]) && !empty($_POST["email"])) {
-        $nome = addslashes($_POST["nome"]);
-        $email = addslashes($_POST["email"]);
-        $mensagem = addslashes($_POST["mensagem"]);
+    //if(isset($_POST["email"]) && !empty($_POST["email"])) {
+    //    $nome = addslashes($_POST["nome"]);
+    //    $email = addslashes($_POST["email"]);
+    //    $mensagem = addslashes($_POST["mensagem"]);
+    //    $to = "vinicius.bohrer@ecomp.ufsm.br";
+    //    $subject = "Contato - Startpage";
+    //    $body = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Mensagem: " . $mensagem;
+    //    // O HEADER TEM QUE COMEÇAR COM FROM, OBRIGATORIAMENTE
+    //    // E O EMAIL TEM QUE PERTENCER AO DOMÍNIO NO QUAL O SCRIPT TA RODANDO
+    //    $header = "From: contato@startpage.com.br\n" . "Reply-To: " . $email . "\n" . "X=Mailer:PHP/" . phpversion();
+    //    echo("From:" . $nome . " (" . $email . ")\n" . "Message:" . $mensagem);
+    //    if(mail($to, $subject, $body, $header)) {
+    //        echo("E-mail enviado com sucesso.")
+    //    } else {
+    //        echo("Erro ao enviar e-mail.")
+    //    }
+    //}
 
-        $to = "vinicius.bohrer@ecomp.ufsm.br";
-        $subject = "Contato - Startpage";
-        $body = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Mensagem: " . $mensagem;
-        // O HEADER TEM QUE COMEÇAR COM FROM, OBRIGATORIAMENTE
-        // E O EMAIL TEM QUE PERTENCER AO DOMÍNIO NO QUAL O SCRIPT TA RODANDO
-        $header = "From: contato@startpage.com.br\n" . "Reply-To: " . $email . "\n" . "X=Mailer:PHP/" . phpversion();
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $mensagem = $_POST["mensagem"];
 
-        echo("From:" . $nome . " (" . $email . ")\n" . "Message:" . $mensagem);
-
-        if(mail($to, $subject, $body, $header)) {
-            echo("E-mail enviado com sucesso.")
-        } else {
-            echo("Erro ao enviar e-mail.")
-        }
-    }
+    echo "<p>From: ".$nome." (".$email.")</p>";
+    echo "<p>Message: ".$mensagem."</p>";
 
 ?>
