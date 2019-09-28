e9771d51ff554f254ee9449535301d0f14d646d3
vbsantos <vbsanttos@gmail.com>
2019-09-28 00:01:53 -0300

    parte de contato funcionando

--- php/contato.php
+++ php/contato.php
@@ -1,28 +1,68 @@
 <?php
 
-    //if(isset($_POST["email"]) && !empty($_POST["email"])) {
-    //    $nome = addslashes($_POST["nome"]);
-    //    $email = addslashes($_POST["email"]);
-    //    $mensagem = addslashes($_POST["mensagem"]);
-    //    $to = "vinicius.bohrer@ecomp.ufsm.br";
-    //    $subject = "Contato - Startpage";
-    //    $body = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Mensagem: " . $mensagem;
-    //    // O HEADER TEM QUE COMEÇAR COM FROM, OBRIGATORIAMENTE
-    //    // E O EMAIL TEM QUE PERTENCER AO DOMÍNIO NO QUAL O SCRIPT TA RODANDO
-    //    $header = "From: contato@startpage.com.br\n" . "Reply-To: " . $email . "\n" . "X=Mailer:PHP/" . phpversion();
-    //    echo("From:" . $nome . " (" . $email . ")\n" . "Message:" . $mensagem);
-    //    if(mail($to, $subject, $body, $header)) {
-    //        echo("E-mail enviado com sucesso.")
-    //    } else {
-    //        echo("Erro ao enviar e-mail.")
-    //    }
-    //}
-
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $mensagem = $_POST["mensagem"];
 
-    echo "<p>From: ".$nome." (".$email.")</p>";
-    echo "<p>Message: ".$mensagem."</p>";
+    // A PARTIR DAQUI SEGUINDO O 
+    //  GITHUB https://github.com/PHPMailer/PHPMailer 
+    //  E O VÍDEO https://www.youtube.com/watch?v=gor2j4muG8A
+    // E VÁRIAS PÁGIAS DO STACK OVERFLOW JUNTAMENTE COM https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require './PHPMailer/src/Exception.php';
+    require './PHPMailer/src/PHPMailer.php';
+    require './PHPMailer/src/SMTP.php';
+
+    $Mailer = new PHPMailer();
+
+    //define que será usado SMTP
+    $Mailer->isSMTP();
+
+    //aceitar caracteres especiais
+    $Mailer->Charser = 'UTF-8';
+
+    //Configurações
+    $Mailer->IsHTML(true);
+    $Mailer->SMTPAuth = true;
+    $Mailer->SMTPSecure = 'ssl';
+
+    //Nome do servidor
+    $Mailer->Host = 'tls://smtp.gmail.com:587';
+    //$Mailer->Port = 587;
+
+    //Porta de saída do e-mail
+    //$Mailer->Port = 3306;
+
+    //Dados do usuário do e-mail de saída
+    $Mailer->Username = 'vbsanttos@gmail.com';
+    $Mailer->Password = 'vbsantos1510!';
+
+    //E-mail remetente
+    $Mailer->From = $email;
+
+    //Nome do remetente
+    $Mailer->FromName = $nome;
+
+    //Assunto da mensagem
+    $Mailer->Subject = "Contato - Startpage";
+
+    //Conteúdo da mensagem
+    $Mailer->Body = $mensagem;
+
+    //Corpo da mensagem em texto
+    $Mailer->AltBody = $mensagem;
+
+    //Destinatário
+    $Mailer->AddReplyTo($email);
+    $Mailer->AddAddress('vbsanttos@gmail.com');
+
+    if($Mailer->Send()) {
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "E-mail NÃO enviado com sucesso!<br>" . $Mailer->ErrorInfo;
+    }
 
 ?>
